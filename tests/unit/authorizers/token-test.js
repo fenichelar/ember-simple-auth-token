7c3b938075a700e2b4b48445ff0a18684252f5c1
José Padilla <jpadilla@webapplicate.com>
2015-04-10 18:20:34 -0400

    Fix failing test

--- tests/unit/authorizers/token-test.js
+++ tests/unit/authorizers/token-test.js
@@ -40,7 +40,7 @@ test('#authorize when authenticated adds token to request', function() {
   expect(3);
 
   App.authorizer.set('session.isAuthenticated', true);
-  App.authorizer.set('session.token', 'secret token!');
+  App.authorizer.set('session.secure.token', 'secret token!');
   App.authorizer.authorize(App.request, {});
 
   ok(setRequestHeader);
@@ -53,7 +53,7 @@ test('#authorize when authenticated adds token to request', function() {
 });
 
 test('#authorize when session does not contain token', function() {
-  App.authorizer.set('session.user_token', null);
+  App.authorizer.set('session.secure.user_token', null);
   App.authorizer.authorize(App.request, {});
 
   ok(!setRequestHeader);
