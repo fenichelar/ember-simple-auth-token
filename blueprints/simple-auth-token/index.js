7ac81aa2a465df3adb92a6a7fc96f524eeabaf0d
José Padilla <jpadilla@webapplicate.com>
2015-06-28 14:12:31 -0400

    Use ember-simple-auth 0.8.0

--- blueprints/simple-auth-token/index.js
+++ blueprints/simple-auth-token/index.js
@@ -3,6 +3,6 @@ module.exports = {
   },
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('ember-simple-auth', '0.8.0-beta.3');
+    return this.addBowerPackageToProject('ember-simple-auth', '0.8.0');
   }
 };
