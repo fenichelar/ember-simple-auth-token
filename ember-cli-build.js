f822b44826280c84376fd596979bed36a1050213
Emmanuel Luciano <emma.luciano11@gmail.com>
2015-12-18 18:42:42 -0400

    Adds ember-cli update

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,11 @@
+/*jshint node:true*/
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-addon');
 var Funnel = require('broccoli-funnel');
+var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
+  var app = new EmberAddon(defaults, {
+     // Add options here
   });
 
   var extraAssets = new Funnel(app.bowerDirectory + '/sinon', {
