4ee07ef546d11172d4a79fb98834401ad6d5a612
thegranddesign <catchall+thegranddesign@livinghighontheblog.com>
2016-11-02 16:44:33 -0700

    Add ability to override Accept header

--- addon/authenticators/jwt.js
+++ addon/authenticators/jwt.js
@@ -305,7 +305,9 @@ export default TokenAuthenticator.extend({
       contentType: 'application/json',
       headers: this.headers,
       beforeSend: (xhr, settings) => {
-        xhr.setRequestHeader('Accept', settings.accepts.json);
+        if(this.headers['Accept'] === null || this.headers['Accept'] === undefined) {
+          xhr.setRequestHeader('Accept', settings.accepts.json);
+        }
 
         if (headers) {
           Object.keys(headers).forEach((key) => {
