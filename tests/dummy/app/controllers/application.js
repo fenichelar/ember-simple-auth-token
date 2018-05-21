a3e83ec96f87331bb05222a5c93789ef37d15849
Alec Fenichel <alec.fenichel@gmail.com>
2018-05-21 13:24:05 -0400

    Display actual config in dummy app

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,10 +1,15 @@
 import Controller from '@ember/controller';
 import { inject } from '@ember/service';
-import { computed } from '@ember/object';
+import { get, computed } from '@ember/object';
 import { getOwner } from '@ember/application';
+import config from '../config/environment'
 
 export default Controller.extend({
   session: inject('session'),
+
+  config: computed(function() {
+    return JSON.stringify(get(config, 'ember-simple-auth-token'), null, '\t');
+  }),
 
   sessionData: computed('session.session.content.authenticated', function() {
     return JSON.stringify(this.get('session.session.content.authenticated'), null, '\t');
