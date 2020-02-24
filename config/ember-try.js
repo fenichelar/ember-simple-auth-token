d3eb13bcf97d670b10f903a253a87b1662c80485
Alec Fenichel <alec.fenichel@gmail.com>
2020-02-24 18:29:56 -0500

    Add all curent ember LTS versions to testing

--- config/ember-try.js
+++ config/ember-try.js
@@ -27,6 +27,30 @@ module.exports = function() {
           }
         },
         {
+          name: 'ember-lts-3.4',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.4.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.8',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.8.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.12',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.12.0'
+            }
+          }
+        },
+        {
           name: 'ember-release',
           npm: {
             devDependencies: {
