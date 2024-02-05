bf3d2c4054ce13cb1333e3673adc2e37a49d3132
AmauryD <deflorenne.amaury@gmail.com>
2024-02-05 20:49:17 +0100

    fix eslint

--- test-app/tests/helpers/destroy-app.js
+++ test-app/tests/helpers/destroy-app.js
@@ -1,5 +1,6 @@
 import { run } from '@ember/runloop';
 
 export default function destroyApp(application) {
+  // eslint-disable-next-line ember/no-runloop
   run(application, 'destroy');
 }
--- test-app/tests/helpers/start-app.js
+++ test-app/tests/helpers/start-app.js
@@ -6,6 +6,7 @@ export default function startApp(attrs) {
   let attributes = Object.assign({}, config.APP);
   attributes = Object.assign(attributes, attrs); // use defaults, but you can override;
 
+  // eslint-disable-next-line ember/no-runloop
   return run(() => {
     const application = Application.create(attributes);
     application.setupForTesting();
