19a5ba88c313f136fb3960d11ac7fcf114326989
Chris Novak <chris.novak@transnexus.com>
2024-08-27 10:02:56 -0700

    move and edit ember-try files

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -1,133 +1,142 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
-const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
-module.exports = async function () {
-  return {
-    scenarios: [
-      {
-        name: 'ember-lts-3.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.4.0',
-          },
+module.exports = async function() {
+  return Promise.all([
+    getChannelURL('release'),
+    getChannelURL('beta'),
+    getChannelURL('canary')
+  ]).then(urls => {
+    return {
+      scenarios: [
+        {
+          name: 'ember-lts-3.28-ember-simple-auth-6.0',
+          npm: {
+            dependencies: {
+              'ember-simple-auth': '^6.0.0'
+            },
+            devDependencies: {
+              'ember-source': '^3.28.0'
+            },
+            ember: {
+              'edition': 'octane'
+            }
+          }
         },
-      },
-      {
-        name: 'ember-lts-3.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.8.0',
-          },
+        {
+          name: 'ember-lts-4.4-ember-simple-auth-6.0',
+          npm: {
+            dependencies: {
+              'ember-simple-auth': '^6.0.0'
+            },
+            devDependencies: {
+              'ember-source': '^4.4.0'
+            },
+            ember: {
+              'edition': 'octane'
+            }
+          }
         },
-      },
-      {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0',
-          },
+        {
+          name: 'ember-lts-4.8-ember-simple-auth-6.0',
+          npm: {
+            dependencies: {
+              'ember-simple-auth': '^6.0.0'
+            },
+            devDependencies: {
+              'ember-source': '^4.8.0'
+            },
+            ember: {
+              'edition': 'octane'
+            }
+          }
         },
-      },
-      {
-        name: 'ember-lts-3.16',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.16.0',
-          },
+        {
+          name: 'ember-lts-4.12-ember-simple-auth-6.0',
+          npm: {
+            dependencies: {
+              'ember-simple-auth': '^6.0.0'
+            },
+            devDependencies: {
+              'ember-source': '^4.12.0'
+            },
+            ember: {
+              'edition': 'octane'
+            }
+          }
         },
-      },
-      {
-        name: 'ember-lts-3.20',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.20.0',
-          },
+        {
+          name: 'ember-lts-5.4-ember-simple-auth-6.0',
+          npm: {
+            dependencies: {
+              'ember-simple-auth': '^6.0.0'
+            },
+            devDependencies: {
+              'ember-source': '^5.4.0'
+            },
+            ember: {
+              'edition': 'octane'
+            }
+          }
         },
-      },
-      {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.0',
-          },
+        {
+          name: 'ember-lts-5.8-ember-simple-auth-6.0',
+          npm: {
+            dependencies: {
+              'ember-simple-auth': '^6.0.0'
+            },
+            devDependencies: {
+              'ember-source': '^5.8.0'
+            },
+            ember: {
+              'edition': 'octane'
+            }
+          }
         },
-      },
-      {
-        name: 'ember-lts-3.28',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
+        {
+          name: 'ember-release-ember-simple-auth-6.0',
+          npm: {
+            dependencies: {
+              'ember-simple-auth': '^6.0.0'
+            },
+            devDependencies: {
+              'ember-source': urls[0]
+            },
+            ember: {
+              'edition': 'octane'
+            }
+          }
         },
-      },
-      {
-        name: 'ember-lts-4.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.4.0',
-          },
+        {
+          name: 'ember-beta-ember-simple-auth-6.0',
+          npm: {
+            dependencies: {
+              'ember-simple-auth': '^6.0.0'
+            },
+            devDependencies: {
+              'ember-source': urls[1]
+            },
+            ember: {
+              'edition': 'octane'
+            }
+          }
         },
-      },
-      {
-        name: 'ember-lts-4.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.8.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-4.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.12.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-5.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~5.4.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-5.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~5.8.0',
-          },
-        },
-      },
-      {
-        name: 'ember-release',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('release'),
-          },
-        },
-      },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta'),
-          },
-        },
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary'),
-          },
-        },
-      },
-      embroiderSafe(),
-      embroiderOptimized(),
-    ],
-  };
+        {
+          name: 'ember-canary-ember-simple-auth-6.0',
+          npm: {
+            dependencies: {
+              'ember-simple-auth': '^6.0.0'
+            },
+            devDependencies: {
+              'ember-source': urls[2]
+            },
+            ember: {
+              'edition': 'octane'
+            }
+          }
+        }
+      ]
+    };
+  });
 };
